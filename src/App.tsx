898bd7d549c0b95f0653b8b8f2f18316900d3098
Kichan <rlcks01537@gmail.com>
2023-06-09 15:40:00 +0900

    Pomodoro Timer

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
-import React from "react";
+import {useEffect,useState,useRef} from "react";
+import {useRecoilState} from "recoil";
+import {timerState} from "./atoms";
 import styled from "styled-components";
 import {motion} from "framer-motion";
 
@@ -22,7 +24,7 @@ const Timer=styled(motion.div)`
   justify-content:center;
 `;
 
-const HourBox=styled(motion.div)`
+const MinuteBox=styled(motion.div)`
   width:180px;
   height:250px;
   background-color:white;
@@ -36,7 +38,7 @@ const HourBox=styled(motion.div)`
   margin-right: 20px;
 `;
 
-const MinuteBox=styled(motion.div)`
+const SecondBox=styled(motion.div)`
   width:180px;
   height:250px;
   background-color:white;
@@ -50,7 +52,7 @@ const MinuteBox=styled(motion.div)`
   margin-left:20px;
 `;
 
-const TimerStateBtn=styled(motion.div)`
+const TimerStateBtn=styled(motion.button)`
   width: 100px;
   height: 100px;
   border-radius: 50px;
@@ -63,6 +65,7 @@ const TimerStateBtn=styled(motion.div)`
   top: 60%;
   left: 50%;
   transform: translate(-50%, -50%);
+  outline:none;
   cursor: pointer;
 
   svg {
@@ -110,34 +113,97 @@ const GoalCheckBox=styled.div`
 `;
 
 function App(){
+  const [timer, setTimer] = useRecoilState(timerState);
+  useEffect(() => {
+    let countdownInterval: number | undefined;
+
+    if (timer.start) {
+      countdownInterval = window.setInterval(() => {
+        if (timer.minute === 0 && timer.second === 0) {
+          setTimer((prevTimer) => ({
+            ...prevTimer,
+            minute: 0,
+            second: 10,
+            round: prevTimer.round + 1,
+            start:false
+          }));
+
+          if (timer.round + 1 === 4) {
+            setTimer((prevTimer) => ({
+              ...prevTimer,
+              goal: prevTimer.goal + 1,
+              round: 0,
+            }));
+          }
+        } else {
+          if (timer.second === 0) {
+            setTimer((prevTimer) => ({
+              ...prevTimer,
+              minute: prevTimer.minute - 1,
+              second: 59,
+            }));
+          } else {
+            setTimer((prevTimer) => ({
+              ...prevTimer,
+              second: prevTimer.second - 1,
+            }));
+          }
+        }
+      }, 1000);
+    }
+
+    return () => clearInterval(countdownInterval);
+  }, [timer.start, timer.minute, timer.second, timer.round, setTimer]);
+  
+  
+  const handleTimerToggle = () => {
+    setTimer((prevTimer) => ({
+      ...prevTimer,
+      start: !prevTimer.start,
+    }));
+  };
+
   return (
     <Wrapper>
       <Pomodoro>Pomodoro</Pomodoro>
       <Timer>
-        <HourBox>10</HourBox>
-
-        <MinuteBox>20</MinuteBox>
+        <MinuteBox>{timer.minute < 10 ? "0" + timer.minute : timer.minute}</MinuteBox>
+        <span style={{ fontSize: 80, color: "white" }}>:</span>
+        <SecondBox>{timer.second < 10 ? "0" + timer.second : timer.second}</SecondBox>
       </Timer>
-      <TimerStateBtn><svg fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
-  <path d="M6.3 2.841A1.5 1.5 0 004 4.11V15.89a1.5 1.5 0 002.3 1.269l9.344-5.89a1.5 1.5 0 000-2.538L6.3 2.84z"></path>
-</svg></TimerStateBtn>
-    <CheckBox>
-      <RoundCheckBox>
-        <span>0/4</span>
-        <span>ROUND</span>
-      </RoundCheckBox>
-      <GoalCheckBox>
-        <span>0/12</span>
-        <span>GOAL</span>
-      </GoalCheckBox>
-    </CheckBox>
+      <TimerStateBtn onClick={handleTimerToggle}>
+      {timer.start ? (
+        <svg
+          fill="currentColor"
+          viewBox="0 0 20 20"
+          xmlns="http://www.w3.org/2000/svg"
+          aria-hidden="true"
+        >
+          <path d="M5.75 3a.75.75 0 00-.75.75v12.5c0 .414.336.75.75.75h1.5a.75.75 0 00.75-.75V3.75A.75.75 0 007.25 3h-1.5zM12.75 3a.75.75 0 00-.75.75v12.5c0 .414.336.75.75.75h1.5a.75.75 0 00.75-.75V3.75a.75.75 0 00-.75-.75h-1.5z"></path>
+        </svg>
+        ) : (
+        <svg
+          fill="currentColor"
+          viewBox="0 0 20 20"
+          xmlns="http://www.w3.org/2000/svg"
+          aria-hidden="true"
+        >
+          <path d="M6.3 2.841A1.5 1.5 0 004 4.11V15.89a1.5 1.5 0 002.3 1.269l9.344-5.89a1.5 1.5 0 000-2.538L6.3 2.84z"></path>
+        </svg>
+        )}
+      </TimerStateBtn>
+      <CheckBox>
+        <RoundCheckBox>
+          <span>{timer.round}/4</span>
+          <span>ROUND</span>
+        </RoundCheckBox>
+        <GoalCheckBox>
+          <span>{timer.goal}/12</span>
+          <span>GOAL</span>
+        </GoalCheckBox>
+      </CheckBox>
     </Wrapper>
   );
 }
 
 export default App;
-
-
-{/* <svg fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg" aria-hidden="true">
-  <path d="M5.75 3a.75.75 0 00-.75.75v12.5c0 .414.336.75.75.75h1.5a.75.75 0 00.75-.75V3.75A.75.75 0 007.25 3h-1.5zM12.75 3a.75.75 0 00-.75.75v12.5c0 .414.336.75.75.75h1.5a.75.75 0 00.75-.75V3.75a.75.75 0 00-.75-.75h-1.5z"></path>
-</svg> */}
--- src/atoms.tsx
+++ src/atoms.tsx
@@ -1,6 +1,12 @@
 import {atom} from "recoil";
 
-export const minuteState=atom({
-  key:"minute",
-  default:0 
-})
+export const timerState = atom({
+  key: "timerState",
+  default: {
+    start: false,
+    minute: 0,
+    second: 10,
+    round: 0,
+    goal: 0,
+  },
+});
